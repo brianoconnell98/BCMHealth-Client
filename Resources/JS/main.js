26fa81d6ab4b7091fbc275dbf5d056f8fcbe96e3
brianoconnell98 <117376663@umail.ucc.ie>
2021-02-25 14:57:36 +0000

    Iteration4.4 - test

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -9,7 +9,7 @@ const URL = "https://bcmhealthserver.herokuapp.com/"
 // async means skip and then come back to it 
 // js usually executes in chronoligical order
 // Patients displaying 
-const getPatientsData = async() => {
+const getData = async() => {
     let patientsData = await axios.get(`${URL}patients`),
        {data} = patientsData
      displayUsers(data)
@@ -20,28 +20,9 @@ const displayUsers = data => {
     data = data.map(user => 
     `
         <h1>${user.name}<h1>
-        <h1>${user.email}<h1>
+        <h1>${user.age}<h1>
     `);
     mainContainer.insertAdjacentHTML("afterbegin", data)
 }
 
-getPatientsData()
-
-// Patients displaying 
-const getPhysiosData = async() => {
-    let physiosData = await axios.get(`${URL}physios`),
-       {data} = physiosData
-     displayUsers(data)
-}
-
-const displayUsers = data => {
-    const mainContainer1 = document.querySelector(".main_container1")
-    data = data.map(user => 
-    `
-        <h1>${user.name}<h1>
-        <h1>${user.location}<h1>
-    `);
-    mainContainer1.insertAdjacentHTML("afterbegin", data)
-}
-
-getPhysiosData()
+getData()
