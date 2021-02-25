12ec7bfb86084d6820d3cbb88cd2e9ee8cf32408
brianoconnell98 <117376663@umail.ucc.ie>
2021-02-25 15:00:00 +0000

    Iteration4.4 - test

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -21,6 +21,7 @@ const displayUsers = data => {
     `
         <h1>${user.name}<h1>
         <h1>${user.email}<h1>
+        <h1>${user.password}<h1>
     `);
     mainContainer.insertAdjacentHTML("afterbegin", data)
 }
